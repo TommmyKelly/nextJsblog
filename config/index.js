c284c02863eabcf89f0c436769b0066a214d5499
TommmyKelly <tommy_kelly@icloud.com>
2021-02-25 01:21:41 +0000

    fixed link

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://yourwebsitename";
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://agitated-chandrasekhar-d50d51.netlify.app";
