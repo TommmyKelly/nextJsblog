41dfc9e2fce635aa33bf53f7209aa9fb101bf801
TommmyKelly <tommy_kelly@icloud.com>
2021-03-08 16:36:47 +0000

    new

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-jsblog-kxfaz1eed-tommmykelly.vercel.app/api/articles";
+  : "https://next-jsblog-kxfaz1eed-tommmykelly.vercel.app/";
