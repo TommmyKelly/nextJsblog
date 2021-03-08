bbc905953e4cb26d30d73823d8eb0031f5528559
TommmyKelly <tommy_kelly@icloud.com>
2021-03-08 16:48:36 +0000

    update

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -32,15 +32,7 @@ export const getStaticProps = async (context) => {
 };
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`${server}/api/articles/`, {
-    method: "GET",
-    headers: {
-      // update with your user-agent
-      "User-Agent":
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-      Accept: "application/json; charset=UTF-8",
-    },
-  });
+  const res = await fetch(`${server}/api/articles`);
 
   const articles = await res.json();
 
--- pages/index.js
+++ pages/index.js
@@ -24,15 +24,7 @@ export default function Home({ articles }) {
 // };
 
 export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/articles`, {
-    method: "GET",
-    headers: {
-      // update with your user-agent
-      "User-Agent":
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
-      Accept: "application/json; charset=UTF-8",
-    },
-  });
+  const res = await fetch(`${server}/api/articles`);
   const articles = await res.json();
 
   return {
