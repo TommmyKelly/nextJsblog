bb1c66230f11c46a59bdea3aa317b8d34f99829d
TommmyKelly <tommy_kelly@icloud.com>
2021-03-08 15:39:38 +0000

    add api

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://6036fe0cfedeaa6bacbfd659--agitated-chandrasekhar-d50d51.netlify.app/";
+  : "https://next-jsblog-kxfaz1eed-tommmykelly.vercel.app/api/articles";
